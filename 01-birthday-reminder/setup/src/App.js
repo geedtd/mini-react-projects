6e5424170db9ee2f46765812765f8560b379cfc4
geedtd <grcazares@gmail.com>
2022-05-21 01:43:22 -0700

    halfway through with birthday project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people,setPeople] = useState(data)
+
+  return <main>
+    <section className="container">
+      <h3>{people.length} Birthdays Today</h3>
+      <List people={people}/>
+      <button onClick={() => console.log('I was clicked')}>
+        Clear All Birthday Reminders
+      </button>
+    </section>
+  </main>;
 }
 
 export default App;

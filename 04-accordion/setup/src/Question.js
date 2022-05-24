31ae6d8ebb9519f394d4bd2dada0fe5ed0bfb9f5
geedtd <grcazares@gmail.com>
2022-05-24 02:26:59 -0700

    added functionality for plus and minus icons on side to toggle between correctly

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -3,12 +3,20 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title, info}) => {
 
   const [showAnswer, setShowAnswer] = useState(false)
+  const [icon, setIcon] = useState(<AiOutlinePlus/>)
+
+  const toggleText = () => {
+    setShowAnswer(!showAnswer)
+    
+  }
 
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn" onClick={() => setShowAnswer(!showAnswer)}>btn</button>
+        <button className="btn" onClick={toggleText} >
+        {showAnswer ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
       </header>
       <p>{(showAnswer? info : '')}</p>
     </article>

370d77c81a64a0c170107fed014cc8721179b93f
geedtd <grcazares@gmail.com>
2022-05-24 16:33:08 -0700

    created UI for user to filter food categories on menu

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -7,13 +7,22 @@ function App() {
   const [menuItems, setMenuItems ] = useState(items)
   const [categories, setCategories ] = useState([])
 
+  const filterItems = (category) => {
+    if (category === 'all'){
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category) 
+    setMenuItems(newItems)
+  }
+
   return <main>
     <section className="menu">
       <div className="title">
       <h2>our menu</h2> 
       <div className="underline"></div>
       </div>
-      <Categories />
+      <Categories filterItems={filterItems}/>
       <Menu items={menuItems}/>
     </section>
   </main>;

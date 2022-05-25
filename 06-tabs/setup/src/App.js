5d098a84d1186777e29266a964d992fe49ce711b
geedtd <grcazares@gmail.com>
2022-05-24 21:03:33 -0700

    done fetching data from api

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,33 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const [loading, setLoading ] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  console.log(jobs);
+  
+  if (loading) {
+    return [
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    ]
+  }
+
+  return <h2>Jobs</h2>
 }
 
 export default App
